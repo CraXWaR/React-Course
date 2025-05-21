1d897bf21a3d57fd4739a8500e3f2f9094537a4a
Emanuil Petrov <craxwar@gmail.com>
2025-05-21 17:40:38 +0300

    Adding second learning project tic-tac-toe

---
+++ tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit() {
+        setIsEditing(true);
+        console.log(isEditing);
+    }
+
+    let playerName = <span className='player-name'>{name}</span>
+
+    if (isEditing) {
+        playerName = <input type='text' required />
+    }
+
+    return (
+        <li>
+            <span className='player'>
+                {playerName}
+                <span className='player-symbol'>{symbol}</span>
+            </span>
+            <button onClick={handleEdit}>Edit</button>
+        </li>
+    );
+}
