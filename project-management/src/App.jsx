9566bfb0d5f299ac5510c2ae1091724c10a4578a
Emanuil Petrov <craxwar@gmail.com>
2025-05-29 17:47:20 +0300

    Adding practice project on useState and component styling

---
+++ project-management/src/App.jsx
@@ -0,0 +1,32 @@
+import Sidebar from "./components/Sidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
+
+function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined, projects: []
+    });
+
+    function handleAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState, selectedProjectId: null,
+            }
+        })
+    }
+
+    let content;
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject/>;
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onAddProject={handleAddProject}/>;
+    }
+
+    return (<main className="h-screen my-8 flex">
+        <Sidebar onAddProject={handleAddProject}/>
+        {content}
+    </main>);
+}
+
+export default App;
