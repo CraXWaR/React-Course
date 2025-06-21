0fde6756c89c1436550ac7a8d7c05819a4a3fdf6
Emanuil Petrov <craxwar@gmail.com>
2025-06-21 12:36:55 +0300

    creating first simple reducer

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,15 +1,30 @@
 import classes from './Counter.module.css';
+import {useDispatch, useSelector} from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const counter = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    const increment = () => {
+        dispatch({type: 'INCREMENT'});
+    };
+
+    const decrement = () => {
+        dispatch({type: 'DECREMENT'});
+    };
+
+    const toggleCounterHandler = () => {
+    };
+
+    return (<main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>-- {counter} --</div>
+        <div>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>);
 };
 
 export default Counter;
