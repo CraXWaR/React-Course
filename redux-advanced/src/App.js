8816b1d7f4eabbe690eb4380a895a9a215597bfe
Emanuil Petrov <craxwar@gmail.com>
2025-06-27 18:03:32 +0300

    finishing the redux-advanced lectures and creating 2 more redux funcions to fetch data and send data to firebase db

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,15 +1,46 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {Fragment, useEffect} from "react"
+
+import Notification from "./components/UI/Notification";
+
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+
+let isInitialized = true;
 
 function App() {
     const isCartVisible = useSelector((state) => state.ui.isVisible);
+    const cart = useSelector(state => state.cart);
+    const notifications = useSelector((state) => state.ui.notification);
 
-    return (<Layout>
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCartData());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (isInitialized) {
+            isInitialized = false;
+            return;
+        }
+
+        if (cart.changed) {
+            dispatch(sendCartData(cart));
+        }
+    }, [cart, dispatch]);
+
+    return (<Fragment>
+        {notifications &&
+            <Notification status={notifications.status} tiitle={notifications.title} message={notifications.message}/>}
+        <Layout>
             {isCartVisible && <Cart/>}
             <Products/>
-        </Layout>);
+        </Layout>
+    </Fragment>);
 }
 
 export default App;
--- redux-advanced/src/store/ui-slice.js
+++ redux-advanced/src/store/ui-slice.js
@@ -2,10 +2,14 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: "ui", initialState: {
-        isVisible: false,
+        isVisible: false, notification: null
     }, reducers: {
         toggleVisible: (state) => {
             state.isVisible = !state.isVisible;
+        }, showNotification: (state, action) => {
+            state.notification = {
+                status: action.payload.status, title: action.payload.title, message: action.payload.message
+            };
         }
     }
 });
