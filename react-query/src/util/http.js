230426e871febb65ba951c1d340aadf5005e71a7
Emanuil Petrov <craxwar@gmail.com>
2025-07-16 18:03:29 +0300

    adding demo project for react query lecture, creating search funcionality with query request

---
+++ react-query/src/util/http.js
@@ -0,0 +1,20 @@
+export async function fetchEvents({signal, searchText}) {
+    let url = 'http://localhost:3000/events';
+
+    if (searchText) {
+        url += `?searchText=${searchText}`;
+    }
+
+    const response = await fetch(url, {signal: signal});
+
+    if (!response.ok) {
+        const error = new Error('An error occurred while fetching the events');
+        error.code = response.status;
+        error.info = await response.json();
+        throw error;
+    }
+
+    const {events} = await response.json();
+
+    return events;
+}
