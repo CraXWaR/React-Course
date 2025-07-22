9314012f42909010cf15ff28d0198d7938163560
Emanuil Petrov <craxwar@gmail.com>
2025-07-22 16:55:23 +0300

    adding dummy comunity page, adding meals grid page, and meal details page

--- foodies-nextJs/app/meals/page.jsx
+++ foodies-nextJs/app/meals/page.jsx
@@ -1,3 +1,20 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
-    return <h1>All Meals</h1>;
+    return (<>
+            <header className={classes.header}>
+                <h1>
+                    Meals created by <span className={classes.highlight}>you</span>
+                </h1>
+                <p>Choose your Meal.</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favourite Meal</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]}/>
+            </main>
+        </>);
 }
--- foodies-nextJs/components/main-header/main-header.jsx
+++ foodies-nextJs/components/main-header/main-header.jsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 
 import logoImg from '../../assets/logo.png';
 import classes from "./main-header.module.css";
 import {MainHeaderBackground} from "@/components/main-header/main-header-background";
+import {usePathname} from "next/navigation";
 
 export function Header() {
+    const path = usePathname();
+
     return (<>
             <MainHeaderBackground/>
             <header className={classes.header}>
@@ -16,8 +21,10 @@ export function Header() {
 
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals" className={classes.link}>Meals</Link></li>
-                        <li><Link href="/community" className={classes.link}>Community</Link></li>
+                        <li><Link href="/meals" className={path.startsWith('/meals') ? classes.active : ''}>Meals</Link>
+                        </li>
+                        <li><Link href="/community"
+                                  className={path.startsWith('/community') ? classes.active : ''}>Community</Link></li>
                     </ul>
                 </nav>
             </header>
